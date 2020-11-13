1f6e3e4b04ff028b9b7c87ebd293493be5db5bf4
Emmanuel Guadalupe <ema_110@outlook.es>
2020-11-13 11:29:51 -0600

    Mas practicas y ejercicios

---
+++ Fprogramacion/Practicas2do/Ejercicio9/sueldo.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void leerDatos(float *, char *);
+void calcularSueldo(float *, float *, char *);
+void mostrarSueldo(float);
+
+void main() {
+	float horas, sueldoB;
+	char catg;
+	
+	leerDatos(&horas, &catg);
+	calcularSueldo(&horas, &sueldoB, &catg);
+	
+	mostrarSueldo(sueldoB);
+}
+
+void leerDatos(float *horas, char *catg) {
+	printf("Ingresa la categoria del trabajador: ");
+	scanf("%c", catg);
+	printf("Ingresa las horas trabajadas esta semana: ");
+	scanf("%f", horas);
+}
+
+void calcularSueldo(float *horas, float *sueldoB, char *catg) {
+	switch (*catg) {
+		case 'a':
+		case 'A':
+			*sueldoB = 7 * (*horas);
+			break;
+		case 'b':
+		case 'B':
+			*sueldoB = 5 * (*horas);
+			break;
+		case 'c':
+		case 'C':
+			*sueldoB = 4 * (*horas);
+			break;
+		case 'd':
+		case 'D':
+			*sueldoB = 3.5 * (*horas);
+			break;
+		default:
+			printf("OPCION NO VALIDA.\n");
+			exit(0);
+			break;
+	}
+}
+
+void mostrarSueldo(float sueldoB) {
+	printf("El sueldo calculado es: %.2f\n", sueldoB);
+}
